5662df2f580fc24edc6d762b08352b1286a582e2
Michał Witek <michal.witek.katowice@gmail.com>
2022-03-20 10:11:50 +0100

    init repository

---
+++ java_script/index.js
@@ -0,0 +1,49 @@
+console.log('Hello JS');
+
+// Variables and primitive types
+
+// let myLet = "myLet";
+// const myConst = 5;
+// const myConst2 = '5';
+// var myVar = true;
+//
+// const myNull = null;
+// const myUndefined = undefined;
+
+// console.log("myLet is type of: " + typeof  myLet);
+// console.log("myConst is type of: " + typeof  myConst);
+// console.log("myConst2 is type of: " + typeof  myConst2);
+// console.log("myNull is type of: " + typeof  myNull);
+// console.log("myUndefined is type of: " + typeof  myUndefined);
+
+// Changing declaration of variable
+
+// let myChange = 7;
+// console.log("myChange is type of: " + typeof  myChange);
+// myChange = '7';
+// console.log("myChange is type of: " + typeof  myChange);
+//
+// const myChange2 = 7;
+// myChange2 = 8;
+
+// let myObject = { x: 7 };
+// console.log(myObject)
+// myObject.x = 8;
+// console.log(myObject)
+// myObject = { y: 9 };
+// console.log(myObject)
+
+// const myObject = { x: 7 };
+// console.log(myObject)
+// myObject.x = 8;
+// console.log(myObject);
+// myObject = { y: 9};
+
+// Hoisting
+
+// console.log(myVar);
+// var myVar = true;
+
+// console.log(myVar);
+// const myVar = true;
+
